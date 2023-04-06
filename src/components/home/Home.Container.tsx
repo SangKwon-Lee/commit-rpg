a5fe5629ceb20ffccee090837b9c090e4b98ea31
SangKwon-Lee <rnrel13@gmail.com>
2023-04-06 10:46:25 +0900

    feat

--- src/components/home/Home.Container.tsx
+++ src/components/home/Home.Container.tsx
@@ -10,29 +10,33 @@ const HomeContainer = () => {
     ignoreQueryPrefix: true,
   });
 
-  // const getToken = useCallback(async () => {
-  //   try {
-  //     const result = await axios.get("https://api.github.com/user", {
-  //       headers: {
-  //         Authorization: `token ${access_token}`,
-  //       },
-  //     });
-  //     console.log(result);
-  //     navigate("/");
-  //   } catch (error) {}
-  // }, []);
+  const getToken = useCallback(async () => {
+    try {
+      const result = await axios.get("https://api.github.com/user", {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      });
+      console.log(access_token);
+      console.log(result);
+      localStorage.setItem("access_token", String(access_token));
+      navigate("/");
+    } catch (error) {}
+  }, []);
 
-  // useEffect(() => {
-  //   getToken();
-  // }, []);
+  useEffect(() => {
+    if (access_token) {
+      getToken();
+    }
+  }, [access_token, getToken]);
 
   return (
     <>
       <div>홈</div>
       <a
         href={
-          // "http://localhost:1337/api/connect/github"
-          "https://github.com/login/oauth/authorize?client_id=8ba7614267b82e92cc58"
+          "http://localhost:1337/api/connect/github"
+          // "https://github.com/login/oauth/authorize?client_id=8ba7614267b82e92cc58"
         }
       >
         로그인
